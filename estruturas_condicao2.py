7498a6ad66a898744b2e544d24e33a270d5dac9b
Eduardo Pacheco <epacheco.dev@gmail.com>
2024-11-16 19:31:15 -0300

    change tempoExperiencia value 1

---
+++ estruturas_condicao2.py
@@ -0,0 +1,8 @@
+tempoExperiencia = 1
+
+if tempoExperiencia < 2:
+    print('Nível de conhecimento Júnior.')
+elif 2 < tempoExperiencia and tempoExperiencia < 5:
+    print('Nível de conhecimento Pleno.')
+else:
+    print('Nível de conhecimento Sênior.')
